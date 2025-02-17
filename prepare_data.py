c56535d5946c32dc16f04c7a3d9ae57f55cd5715
Nam Fam <102389548+namwasinyourheart@users.noreply.github.com>
2025-02-18 05:33:28 +0700

    update src

--- prepare_data.py
+++ prepare_data.py
@@ -1,7 +1,59 @@
-import torch
+import os
+from functools import partial
+import argparse
+
+import joblib
 import numpy as np
+import torch
 from datasets import load_dataset
+from hydra import compose, initialize
+from hydra.utils import instantiate
+from omegaconf import OmegaConf
 from transformers import AutoTokenizer
+
+from src.utils.exp_utils import create_exp_dir
+from src.utils.log_utils import init_logging, setup_logger
+
+
+def load_cfg(config_path, override_args=None, print_cfg=True):
+
+    """
+    Load a configuration file using Hydra and OmegaConf.
+    
+    Args:
+        config_path (str): Path to the configuration file.
+        override_args (list, optional): List of arguments to override configuration values.
+
+    Returns:
+        cfg: Loaded configuration object.
+    """
+
+    override_args = override_args or []
+    config_path = os.path.normpath(config_path)
+    
+    if not os.path.isfile(config_path):
+        raise FileNotFoundError(f"Configuration file not found at: {config_path}")
+    
+    config_dir = os.path.dirname(config_path)
+    config_fn = os.path.splitext(os.path.basename(config_path))[0]
+    
+    try:
+        with initialize(version_base=None, config_path=config_dir):
+            cfg = compose(config_name=config_fn, overrides=override_args)
+    except Exception as e:
+        raise RuntimeError(f"Failed to load configuration from {config_path}: {e}")
+    
+    assert os.path.basename(config_path).replace('.yaml', '') == cfg.exp_manager.exp_name, \
+    f"Config file name '{os.path.basename(config_path)}' does not match experiment name '{cfg.exp_manager.exp_name}' in the config."
+
+    if print_cfg:
+        print(OmegaConf.to_yaml(cfg))
+    
+    exp_args = cfg.exp_manager
+    data_args = cfg.prepare_data
+    model_args = cfg.prepare_model
+
+    return cfg, exp_args, data_args, model_args
 
 def load_and_preprocess_data(dataset_name="go_emotions", model_name="bert-large-cased"):
     dataset = load_dataset(dataset_name, "raw")["train"]
@@ -26,3 +78,239 @@ def load_and_preprocess_data(dataset_name="go_emotions", model_name="bert-large-
     
     dataset = dataset.map(preprocess_data, batched=True, batch_size=128)
     return dataset, tokenizer, id2label, label2id
+
+import pandas as pd
+from datasets import Dataset, DatasetDict
+
+def create_dataset_dict(data_path, 
+                        do_split: bool=True, 
+                        val_ratio: float=0.25,
+                        test_ratio: float=0.2,
+                        seed: int = 42):
+    df = pd.read_csv(data_path)
+    hf_dataset = Dataset.from_pandas(df)
+
+  
+    if do_split:
+        # Splitting the dataset
+        train_test_split = hf_dataset.train_test_split(test_size=test_ratio, seed=seed)
+        train_val_split = train_test_split["train"].train_test_split(test_size=val_ratio, seed=seed)  # 0.25 x 0.8 = 0.2
+    
+        # Create DatasetDict
+        dataset_dict = DatasetDict({
+            "train": train_val_split["train"],
+            "val": train_val_split["test"],
+            "test": train_test_split["test"]
+        })
+        
+    else:
+        dataset_dict = DatasetDict({
+            "train": hf_dataset
+        })
+    return dataset_dict
+
+
+def process_data(example, tokenizer, text_col, do_tokenize, padding, max_length):
+    example['text'] = example[text_col]
+    if do_tokenize:
+        tokenized_text = tokenizer(
+            example[text_col], 
+            truncation=True, 
+            padding=padding,
+            # add_special_tokens=True,
+            max_length=max_length
+        )
+
+        example['input_ids'] = tokenized_text['input_ids']
+        example['attention_mask'] = tokenized_text['attention_mask']
+    return tokenized_text
+
+def save_dataset(dataset, save_path):
+    joblib.dump(dataset, save_path)
+
+
+def show_dataset_examples(dataset_dict):
+    """
+    Prints the length, columns, shape of columns, and an example from each split of a DatasetDict (train, val, test).
+
+    Parameters
+    ----------
+    dataset_dict : datasets.DatasetDict
+        A DatasetDict containing train, val, and test splits.
+
+    Returns
+    -------
+    None
+    """
+    for split_name, dataset in dataset_dict.items():
+        # Get the length and columns of the current split
+        dataset_length = len(dataset)
+        dataset_columns = dataset.column_names
+
+        print(f"\nSplit: {split_name}")
+        print(f"Number of Examples: {dataset_length}")
+        print(f"Columns: {dataset_columns}")
+
+
+        import numpy as np
+
+        # Get a random index using numpy
+        random_index = np.random.randint(0, len(dataset))
+
+        # Get the first example from the current split
+        example = dataset[random_index]
+
+        # Calculate the shape of each column
+        print("Shapes:")
+        for column_name in dataset_columns:
+            if column_name in dataset[random_index]:
+                col_data = dataset[column_name]
+                if isinstance(col_data[random_index], list):  # Multi-dimensional data (e.g., tokenized inputs)
+                    print(f"  {column_name}: [{len(col_data)}, {len(col_data[random_index])}]")
+                else:  # Single-dimensional data (e.g., strings)
+                    print(f"  {column_name}: [{len(col_data)}]")
+
+        
+        
+
+        print("An example:")
+        for key, value in example.items():
+            print(f"  {key}: {value}")
+
+    # print("\n" + "-" * 24 + "\n")
+
+
+def prepare_data(exp_args, data_args, model_args):
+
+    if data_args.dataset.already_datasetdict:
+        dataset_dict = load_dataset(data_args.dataset.hf_dataset_path,
+                                    data_args.dataset.hf_dataset_subset_name,
+                                    trust_remote_code=True)
+
+        if data_args.dataset.only_use_train_split:
+            dataset_dict = dataset_dict['train']
+            
+            # Splitting the dataset
+            train_test_split = dataset_dict.train_test_split(test_size=data_args.dataset.val_ratio, 
+                                                             seed=exp_args.seed
+                                                             )
+            train_val_split = train_test_split["train"].train_test_split(test_size=data_args.dataset.val_ratio, 
+                                                                         seed=exp_args.seed
+                                                                         )  # 0.25 x 0.8 = 0.2
+            dataset_dict = DatasetDict({
+                "train": train_val_split["train"],
+                "val": train_val_split["test"],
+                "test": train_test_split["test"]
+            })
+
+            # Create DatasetDict
+            dataset_dict = DatasetDict({
+                "train": train_val_split["train"],
+                "val": train_val_split["test"],
+                "test": train_test_split["test"]
+            })
+        
+    else:    
+        dataset_dict = create_dataset_dict(data_args.dataset.csv_dataset_path, 
+                                           data_args.dataset.do_split, 
+                                           data_args.dataset.val_ratio, 
+                                           data_args.dataset.test_ratio, 
+                                           exp_args.seed)
+
+
+                        
+
+    # If subset_ratio is set and valid, apply subset
+    if data_args.dataset.subset_ratio and 0 < data_args.dataset.subset_ratio < 1:
+        
+        dataset_dict = DatasetDict({
+            split: dataset_dict[split].shuffle(seed=42).select(range(int(len(dataset_dict[split]) * data_args.dataset.subset_ratio)))
+            for split in dataset_dict.keys()
+        })  
+
+    from src.utils.model_utils import load_tokenizer
+    tokenizer = load_tokenizer(data_args, model_args)
+
+    print("TOKENIZER CONFIGS:\n", tokenizer)
+
+    _process_data = partial(
+        process_data,
+        tokenizer = tokenizer,
+        text_col=data_args.dataset.text_col, 
+        do_tokenize=data_args.tokenizer.do_tokenize, 
+        padding=data_args.tokenizer.padding,
+        max_length=data_args.tokenizer.max_length
+    )
+
+    dataset = dataset_dict.map(
+        _process_data, 
+         batched=False, 
+        #  remove_columns=dataset_dict['train'].column_names
+    )
+
+    if data_args.dataset.do_save:
+        save_path = data_args.dataset.prepared_data_path
+        save_dataset(dataset, save_path)
+
+    return dataset, save_path
+
+def setup_environment() -> None:
+    from dotenv import load_dotenv
+    # print("SETTING UP ENVIRONMENT...")
+    _ = load_dotenv()
+
+def main():
+
+    # Setup logging
+    logger = setup_logger()
+
+    # Setup environment
+    logger.info("SETTING UP ENVIRONMENT...")
+    setup_environment()
+
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Load experiment configurations.')
+    parser.add_argument(
+        '--config_path',
+        type=str,
+        required=True,
+        help='Path to the configuration file for the experiment.'
+    )
+
+    args, override_args = parser.parse_known_args()
+
+    # Load configuration
+    logger.info("LOADING CONFIGURATIONS...")
+    cfg, exp_args, data_args, model_args = load_cfg(config_path=args.config_path, override_args=override_args)
+    
+    # Create experiment directories
+    # logger.info("CREATING DIRECTORIES...")""
+    exp_name = cfg.exp_manager.exp_name
+    (exp_dir, exp_data_dir, exp_checkpoints_dir, exp_results_dir) = create_exp_dir(exp_name)
+
+    if data_args.dataset.is_prepared:
+        # Get the path to the processed data
+        prepared_data_path = os.path.normpath(data_args.dataset.prepared_data_path)
+        
+        # Check if the processed data exists
+        if not os.path.isfile(prepared_data_path):
+            raise FileNotFoundError(f"Processed data not found at: {prepared_data_path}")
+        
+        # Load the dataset
+        logger.info("LOADING PROCESSED DATASET...")
+        dataset = joblib.load(prepared_data_path)
+    else:
+        # Prepare dataset
+        logger.info("PREPARING DATASET...")
+        # from prepare_data import prepare_data
+        dataset, processed_data_path = prepare_data(exp_args, data_args, model_args)
+
+    # print(dataset)
+    
+    # Show dataset examples
+    show_dataset_examples(dataset)
+
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -1,8 +1,7 @@
 import os
 import argparse
+import joblib
 
-import torch
-from transformers import AutoTokenizer
 
 from dotenv import load_dotenv
 
@@ -10,12 +9,13 @@ from hydra import initialize, compose
 from hydra.utils import instantiate
 from omegaconf import OmegaConf
 
-from src.teacher_model import load_teacher_model
-from src.student_model import load_student_model
-from src.trainer import DistillationTrainer
+from transformers import DataCollatorWithPadding
+from src.utils.model_utils import load_tokenizer, load_model
+from src.trainer import DistillationTrainingArguments, DistillationTrainer
 
-from prepare_data import load_dataset
-from src.configs import training_args
+from prepare_data import prepare_data, show_dataset_examples
+
+from src.metrics import compute_metrics
 
 from src.utils.log_utils import setup_logger
 from src.utils.exp_utils import setup_environment, create_exp_dir
@@ -64,8 +64,9 @@ def load_cfg(config_path, override_args=None, print_cfg=True):
     exp_args = cfg.exp_manager
     data_args = cfg.prepare_data
     model_args = cfg.prepare_model
+    train_args = cfg.train
 
-    return cfg, exp_args, data_args, model_args
+    return cfg, exp_args, data_args, model_args, train_args
 
 
 
@@ -92,7 +93,7 @@ def main():
 
     # Load configuration
     logger.info("LOADING CONFIGURATIONS...")
-    cfg, exp_args, data_args, model_args = load_cfg(config_path=args.config_path, override_args=override_args)
+    cfg, exp_args, data_args, model_args, train_args = load_cfg(config_path=args.config_path, override_args=override_args)
     
     # Create experiment directories
     # logger.info("CREATING DIRECTORIES...")""
@@ -100,25 +101,148 @@ def main():
     (exp_dir, exp_data_dir, exp_checkpoints_dir, exp_results_dir) = create_exp_dir(exp_name)
 
     # Load dataset
-    from prepare_data import load_and_preprocess_data
-    dataset, tokenizer, id2label, label2id = load_and_preprocess_data(data_args.dataset.dataset_name, 
-                                                                      model_args.pretrained_model_name_or_path)
+    if data_args.dataset.is_prepared:
+        # Get the path to the processed data
+        prepared_data_path = os.path.normpath(data_args.dataset.prepared_data_path)
+        
+        # Check if the processed data exists
+        if not os.path.isfile(prepared_data_path):
+            raise FileNotFoundError(f"Processed data not found at: {prepared_data_path}")
+        
+        # Load the dataset
+        logger.info("LOADING PROCESSED DATASET...")
+        dataset = joblib.load(prepared_data_path)
+    else:
+        # Prepare dataset
+        logger.info("PREPARING DATASET...")
+        dataset, prepared_data_path = prepare_data(exp_args, data_args, model_args)
 
-    print(dataset)
+
+    # Show dataset examples
+    show_dataset_examples(dataset)
+
+
+    def get_data_subset(n_samples, dataset, seed):
+        if n_samples == -1:
+            subset = dataset
+        else:
+            subset = dataset.shuffle(seed=seed)
+            subset = subset.select(range(n_samples))
+
+        return subset
+
+    if train_args.train_n_samples:
+        # train_ds = train_ds.shuffle(seed=exp_args.seed).select(range(train_args.train_n_samples))
+        train_ds = get_data_subset(train_args.train_n_samples, dataset['train'], exp_args.seed)
+
+    if train_args.val_n_samples:
+        val_ds = get_data_subset(train_args.val_n_samples, dataset['val'], exp_args.seed)
+
+    if train_args.test_n_samples:
+        test_ds = get_data_subset(train_args.test_n_samples, dataset['test'], exp_args.seed)
+
+
     # Tokenizer
-
+    tokenizer = load_tokenizer(data_args, model_args)
 
     # Load model
+    labels = dataset["train"].features["label"].names
+    num_labels = len(labels)
+    label2id, id2label = dict(), dict()
+    for i, label in enumerate(labels):
+        label2id[label] = str(i)
+        id2label[str(i)] = label
+    
+    teacher_model = load_model(model_args.teacher_model_name_or_path, num_labels, id2label, label2id)
+    student_model = load_model(model_args.student_model_name_or_path, num_labels, id2label, label2id)
 
+    teacher_model.config.pad_token_id = tokenizer.pad_token_id
+    student_model.config.pad_token_id = tokenizer.pad_token_id
+
+    # Data Collator
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     # Training arguments
-
-
+    training_args = instantiate(train_args.training_args, 
+                                output_dir=exp_checkpoints_dir, 
+                                # run_name=wandb.run.name
+                    )
     # Trainer
+    trainer = DistillationTrainer(
+        student_model,
+        training_args,
+        teacher_model=teacher_model,
+        train_dataset=train_ds,
+        eval_dataset=val_ds,
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+    )
 
-    # Train model
-    # logger.info("STARTING TRAINING...")
-    # trainer.train()
+    # WandB
+
+    import wandb
+    wandb.init(
+        project=cfg.exp_manager.wandb.project,
+        # name = cfg.exp_manager.exp_name
+    )
+    all_metrics = {"run_name": wandb.run.name}
+    #  TRAINING
+    if training_args.do_train:
+        logger.info("TRAINING...")
+ 
+
+        if training_args.resume_from_checkpoint:
+            checkpoint = training_args.resume_from_checkpoint
+        # elif last_checkpoint:
+        #     checkpoint = last_checkpoint
+            train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        else:
+            train_result = trainer.train()
+
+        metrics = train_result.metrics
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+        all_metrics.update(metrics)
+        logger.info("TRAINING COMPLETED.")
+
+
+        # Save model
+        logger.info("SAVING MODEL...")
+        trainer.save_model()
+        logger.info("MODEL SAVED SUCCESSFULLY.")
+
+
+    # EVALUATION
+    if training_args.do_eval:
+        logger.info("EVALUATING...")
+        metrics = trainer.evaluate(
+            eval_dataset=val_ds, 
+            metric_key_prefix="eval")
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+        all_metrics.update(metrics)
+
+
+    # PREDICTION
+    if training_args.do_predict:
+        logger.info("PREDICTING...")
+        predictions = trainer.predict(
+            test_dataset=test_ds,
+            metric_key_prefix='test'
+        )
+        trainer.save_predictions(predictions)
+        metrics = predictions.metrics
+        all_metrics.update(metrics)
+
+
+    import json
+    
+    if (training_args.do_train or training_args.do_eval or training_args.do_predict):
+            with open(os.path.join(exp_results_dir, "metrics.json"), "w") as fout:
+                fout.write(json.dumps(all_metrics))
 
 
     # # Evaluate model
